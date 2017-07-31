e25707610a3c61dac2b8e873a718592679902474
Turn-up <dev@goree.in>
2017-07-31 21:57:50 +0900

    dp_20170731

--- BaekjoonOnlineJudge/dynamic_programming/psh_2579/Main.java
+++ BaekjoonOnlineJudge/dynamic_programming/psh_2579/Main.java
@@ -9,27 +9,30 @@ public class Main {
 	private static BufferedReader br;
 	private static int size;
 	private static int n[];
-	private static long d[];
+	private static long d[][];
 	
 	public static void main(String[] args) throws IOException{
 		br = new BufferedReader(new InputStreamReader(System.in));
 		size = Integer.parseInt(br.readLine());
 		n = new int[size+1];
-		d = new long[size+1];
+		d = new long[size+1][3];
 
 		for (int i = 1; i <= size; i++) {
 			n[i] = Integer.parseInt(br.readLine());
 		}
 		
-		d[1] = n[1];
-		d[2] = d[1] + n[1];
-		d[3] = n[3] + max(n[1] ,n[2]);
+		d[1][1] = n[1];
+
+		d[2][1] = n[2];
+		d[2][2] = d[1][1] + n[2];
 		
-		for (int i = 4; i <= size; i++) {
-			d[i] = max(d[i-2], d[i-3]+n[i-1]);
+		for (int i = 3; i <= size; i++) {
+			for (int j = 1; j < 3; j++) {
+				d[i][j] = d[i-j][3-j]+ n[i];
+			}
 		}
 		
-		System.out.println(d[size]);
+		System.out.println(max(d[size][1], d[size][2]));
 		
 	}
 	
