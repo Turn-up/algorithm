77075c6194e716f1803d89a22f445b08c1250864
psh <shcjswo89@naver.com>
2017-07-31 22:56:07 +0900

    계단오르기(dp)

--- BaekjoonOnlineJudge/dynamic_programming/psh_2579/Main.java
+++ BaekjoonOnlineJudge/dynamic_programming/psh_2579/Main.java
@@ -3,7 +3,12 @@ package psh_2579;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+/**
+ * 계단 오르기 dp
+ * 
+ * https://www.acmicpc.net/problem/2579
+ * 
+ * */
 public class Main {
 
 	private static BufferedReader br;
@@ -13,30 +18,36 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		br = new BufferedReader(new InputStreamReader(System.in));
-		size = Integer.parseInt(br.readLine());
-		n = new int[size+1];
-		d = new long[size+1][3];
+		size = Integer.parseInt(br.readLine()); //사이즈
+		n = new int[size+1]; //계단 값 저장
+		d = new long[size+1][3]; // dp 계산 저장
 
+		/*계단값 초기화 진행*/
 		for (int i = 1; i <= size; i++) {
 			n[i] = Integer.parseInt(br.readLine());
 		}
 		
+		/*계산을 위한 최소 정의*/
 		d[1][1] = n[1];
+		d[1][2] = n[1];
 
 		d[2][1] = n[2];
 		d[2][2] = d[1][1] + n[2];
 		
-		for (int i = 3; i <= size; i++) {
-			for (int j = 1; j < 3; j++) {
-				d[i][j] = d[i-j][3-j]+ n[i];
+		for (int i = 3; i <= size; i++) { //각각의 계단
+			for (int j = 2; j > 0; j--) { //몇번째 밟은 것인지
+				if ( j == 2)
+					d[i][2] = longValueCompare(d[i-1][1], d[i-2][2])+ n[i]; //2번째에 밟혔을 때
+				else 
+					d[i][1] = d[i-2][2]+ n[i]; //1번째로 밟혔을 때
 			}
 		}
 		
-		System.out.println(max(d[size][1], d[size][2]));
+		System.out.println(longValueCompare(d[size][1], d[size][2]));
 		
 	}
-	
-	public static long max (long value1, long value2) {
+	/*비교 출력*/
+	public static long longValueCompare (long value1, long value2) {
 		return value1 > value2 ? value1: value2;
 	}
 }
