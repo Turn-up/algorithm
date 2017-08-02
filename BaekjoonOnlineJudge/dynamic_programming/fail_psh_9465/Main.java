a4130bbf818a422ebf1dd1fe228ba210da1db5b7
Turn-up <dev@goree.in>
2017-08-02 09:18:52 +0900

    스티커_실패

--- BaekjoonOnlineJudge/dynamic_programming/fail_psh_9465/Main.java
+++ BaekjoonOnlineJudge/dynamic_programming/fail_psh_9465/Main.java
@@ -1,4 +1,4 @@
-package psh_9465;
+package fail_psh_9465;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,36 +15,33 @@ public class Main {
 	private static Scanner in;
 	private static int testCase;
 	private static int size;
-	private static int n[];
+	private static int n[][];
 	private static int d[][];
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		in = new Scanner(System.in);
 		testCase = in.nextInt();
 		size = in.nextInt();
-		n = new int[size+1];
-		d = new int[3][size+1];
+		n = new int[3][size + 1];
+		d = new int[3][size + 1];
 		
-		for (int j = 1; j < size+1; j++) {
-			n[j] = in.nextInt();
-		}
-		
-		d[1][0] = 0;
-		d[1][1] = n[1];
-
-		d[size+1][0] = 0;
-		d[size+1][1] = n[size+1];
-		
-		for (int i = 2; i < size+1; i++) {
-			for (int j = 0; j < 2; j++) {
-				if (j == 0)
-					d[i][j] = intValueCompare(d[i-1][1], d[i-1][0]);
+		for (int i = 1; i < size + 1; i++) {
+			for (int j = 1; j < size + 1; j++) {
+				n[i][j] = in.nextInt();
 			}
 		}
-		
+
+		for (int i = 2; i < size + 1; i++) {
+			for (int j = 0; j < 2; j++) {
+				if (j == 0)
+					d[i][j] = intValueCompare(d[i - 1][1], d[i - 1][0]);
+			}
+		}
+
 	}
-	/*비교 출력*/
-	public static int intValueCompare (int value1, int value2) {
-		return value1 > value2 ? value1: value2;
+
+	/* 비교 출력 */
+	public static int intValueCompare(int value1, int value2) {
+		return value1 > value2 ? value1 : value2;
 	}
 }
