52a22ab2ade26eb66dd6952956b5770f675ceefb
Turn-up <dev@goree.in>
2017-08-07 11:18:15 +0900

    연결요소 - 그래프

--- BaekjoonOnlineJudge/graph/psh_11724/Main.java
+++ BaekjoonOnlineJudge/graph/psh_11724/Main.java
@@ -13,37 +13,42 @@ public class Main {
 	private static ArrayList<ArrayList<Integer>> a;
 	private static int d[];
 	private static int ans;
-	
+	private static int N;
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();// 정점의 개수
+		N = in.nextInt();// 정점의 개수
 		int M = in.nextInt();// 간선의 개수
 		a = new ArrayList<>();
-		d = new int[N+1];
+		d = new int[N + 1];
 		ans = 0;
-		
-		for (int i=0; i<N; i++) {
+
+		for (int i = 0; i < N+1; i++) {
 			a.add(new ArrayList<>());
 		}
-		
-		for (int i=1; i<M+1; i++) {
-//			a[in.nextInt()] = in.nextInt(); 
+
+		for (int i = 0; i < M; i++) {
+			a.get(in.nextInt()).add(in.nextInt());
 		}
-		
-		for (int i=1; i<N; i++) {
-			if (d[i] == 0){
+
+		for (int i = 1; i < N+1; i++) {
+			if (d[i] == 0) {
 				dfs(i);
 				ans++;
 			}
-			
+
 		}
 		System.out.println(ans);
 	}
-	
-	public static void dfs(int x){
-		if (d[x] == 0) {
-			d[x] = 1;
-//			dfs(a[x]);
+
+	public static void dfs(int x) {
+		d[x] = 1;
+		for (int i = 0; i < a.get(x).size(); i++) {
+			int y = a.get(x).get(i);
+			if (d[y] == 0) {
+				d[y] = 1;
+				dfs(y);
+			}
 		}
 	}
 }
