4ce006f7a3c7816759d962918d1a0565ad250cd2
student <student@M150124>
2017-08-07 17:47:07 +0900

    반복수열-그래프

---
+++ BaekjoonOnlineJudge/graph/psh_2331/Main.java
@@ -0,0 +1,23 @@
+package psh_2331;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * 문제        : https://www.acmicpc.net/problem/2331
+ * 문제 종류  : 반복수열 - 그래프 
+ * 작성자     : 박성훈
+ * 작성일     : 2017-08-07
+ * */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int p = in.nextInt();
+		ArrayList<Integer> d = new ArrayList<>();
+		
+		
+		
+	}
+}
