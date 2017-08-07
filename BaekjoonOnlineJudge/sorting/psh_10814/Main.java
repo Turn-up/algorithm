12688033e5ef99d24f2009d6e2b63cf4ba84efe0
student <student@M150124>
2017-08-07 17:43:23 +0900

    나이순 정렬- 정렬

--- BaekjoonOnlineJudge/sorting/psh_10814/Main.java
+++ BaekjoonOnlineJudge/sorting/psh_10814/Main.java
@@ -1,6 +1,7 @@
 package psh_10814;
 
 import java.util.Scanner;
+import java.util.TreeSet;
 
 /*
  * 문제        : https://www.acmicpc.net/problem/10814
@@ -9,26 +10,46 @@ import java.util.Scanner;
  * 작성일     : 2017-08-07
  * */
 public class Main {
-	
+
 	private static int size;
-	
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		size = in.nextInt();
-		MemberInfo[] memberInfo = new MemberInfo[size];
-
-		for (int i=0; i<size; i++) {
-			memberInfo[i].number = i;
-			memberInfo[i].age = in.nextInt();
-			memberInfo[i].name = in.next();
+		TreeSet<MemberInfo> memberInfoTS = new TreeSet<>();
+		
+		for (int i = 0; i < size; i++) {
+			memberInfoTS.add(new MemberInfo(in.nextInt(),i, in.next()));
 		}
+
+		for (MemberInfo i : memberInfoTS)
+			System.out.println(i.age + " " + i.name);
+
 	}
-	 
-	class MemberInfo{
-		int age;
-		int number;
-		String name;
+
+	static class MemberInfo implements Comparable<MemberInfo>{
+		int age = 0;
+		int number = 0;
+		String name = "";
+	
+		public MemberInfo(int age, int number, String name) {
+			this.age = age;
+			this.number = number;
+			this.name = name;
+		}
+		
+		@Override
+		public int compareTo(MemberInfo m) {
+			if (age > m.age) return 1;
+			else if (age == m.age) {
+				if (number > m.number)
+					return 1;
+				else
+					return -1;
+			}
+			else 
+				return -1;
+		}
+		
 	}
 }
-
-
