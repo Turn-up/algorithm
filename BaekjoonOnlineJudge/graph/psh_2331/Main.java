6e7eb4facf8007c26b248217a618f8b3ddd6d438
ssunghooni <shcjswo89@naver.com>
2017-08-08 17:55:25 +0900

    반복수열 -그래프

--- BaekjoonOnlineJudge/graph/psh_2331/Main.java
+++ BaekjoonOnlineJudge/graph/psh_2331/Main.java
@@ -10,14 +10,30 @@ import java.util.Scanner;
  * ¿€º∫¿œ     : 2017-08-07
  * */
 public class Main {
-
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int a = in.nextInt();
-		int p = in.nextInt();
+		String p = in.next();
 		ArrayList<Integer> d = new ArrayList<>();
+		d.add(MyStringToInteger(p));
 		
+		System.out.println(a);
 		
+		while(true) {
+			d.add(MyStringToInteger(in.next()));
+			
+			in.next();
+		}
 		
+	}
+	
+	public static int MyStringToInteger(String value) {
+		int count = value.length();
+		int result = 0;
+		for (int i=0; i<count; i++) {
+			result += (int)(value.charAt(i)-'0') * (int)(value.charAt(i)-'0'); 
+		}
+		return result;
 	}
 }
