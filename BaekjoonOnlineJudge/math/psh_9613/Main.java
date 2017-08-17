102bf9fdaa02166894a76c5005053ce63962cf13
ssunghooni <shcjswo89@naver.com>
2017-08-17 10:18:49 +0900

    gcd - 수학

--- BaekjoonOnlineJudge/math/psh_9613/Main.java
+++ BaekjoonOnlineJudge/math/psh_9613/Main.java
@@ -3,26 +3,32 @@ package psh_9613;
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int testCase = in.nextInt();
-		
-		for (int i=0; i<testCase; i++) {
-			int count = in.nextInt();
-			int a[] = new int[count];
-			for (int j=0; j<count; j++) {
-				a[j] = in.nextInt();
-			}
-			
+	
+	public static int gcm(int v1, int v2) {
+		if (v2 == 0)
+			return v1;
+		else {
+			return gcm(v2,v1%v2);
 		}
 	}
 	
-//	public int gcm(int v1, int v2) {
-//		if (v2 == 0)
-//			return v1;
-//		else {
-//			v1 = v2;
-//		}
-//	}
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int testCase = in.nextInt();
+
+		for (int i = 0; i < testCase; i++) {
+			int count = in.nextInt();
+			int a[] = new int[count];
+			int result = 0;
+			for (int j = 0; j < count; j++) {
+				a[j] = in.nextInt();
+			}
+			for (int n = 0; n < count; n++) {
+				for (int k = n + 1; k < count; k++) {
+					result += gcm(a[n], a[k]);
+				}
+			}
+			System.out.println(result);
+		}
+	}
 }
