8a4acf3d5101f4632e30b131f8096bb491dd5bbe
Turn-up <dev@goree.in>
2017-08-17 19:49:41 +0900

    그래프

--- BaekjoonOnlineJudge/graph/psh_11724/Main.java
+++ BaekjoonOnlineJudge/graph/psh_11724/Main.java
@@ -1,54 +1,128 @@
 package psh_11724;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 /*
- * 문제        : https://www.acmicpc.net/problem/11724
- * 문제 종류  : 그래프 -연결요소
+ * 문제       : https://www.acmicpc.net/problem/2146
+ * 문제 종류  : 다리 만들기 - 그래프
  * 작성자     : 박성훈
- * 작성일     : 2017-08-06
+ * 작성일     : 2017-08-10
  * */
+class PairXY {
+	int x;
+	int y;
+
+	PairXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
 public class Main {
-	private static ArrayList<ArrayList<Integer>> a;
-	private static int d[];
-	private static int ans;
 	private static int N;
+	private static int a[][];
+	private static int distance[][];
+	private static int land[][];
+	private static int dx[] = { 0, 0, 1, -1 };
+	private static int dy[] = { 1, -1, 0, 0 };
+	private static int cnt;
+	
+
+	public static void bfs(int i, int j, int cnt) {
+		Queue<PairXY> q = new LinkedList<>();
+		q.add(new PairXY(i, j));
+		land[i][j] = cnt;
+		
+
+		/** 그룹 만들기 */
+		while (!q.isEmpty()) {
+			PairXY xy = q.remove();
+			for (int k = 0; k < 4; k++) {
+				int nx = xy.x + dx[k];
+				int ny = xy.y + dy[k];
+				
+				if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+					if (a[nx][ny] == 1 && land[nx][ny] == 0) {
+						q.add(new PairXY(nx, ny));
+						land[nx][ny] = cnt;
+					}
+				}
+			}
+		}
+		
+		long a = Long.valueOf("100");
+	}
+
+	public static void broadBfs() {
+		Queue<PairXY> q = new LinkedList<>();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				distance[i][j] = -1;
+				if (a[i][j] == 1) {
+					distance[i][j] = 0;
+					q.add(new PairXY(i, j));
+				}
+			}
+		}
+		while (!q.isEmpty()) {
+			PairXY xy = q.remove();
+			for (int k = 0; k < 4; k++) {
+				int nx = xy.x + dx[k];
+				int ny = xy.y + dy[k];
+
+				if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+					if (distance[nx][ny] == -1) {
+						q.add(new PairXY(nx, ny));
+						distance[nx][ny] = distance[xy.x][xy.y] + 1;
+						land[nx][ny] = land[xy.x][xy.y];
+					}
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		N = in.nextInt();// 정점의 개수
-		int M = in.nextInt();// 간선의 개수
-		a = new ArrayList<>();
-		d = new int[N + 1];
-		ans = 0;
+		N = in.nextInt();
+		a = new int[N][N];
+		distance = new int[N][N];
+		land = new int[N][N];
 
-		for (int i = 0; i < N+1; i++) {
-			a.add(new ArrayList<>());
-		}
-
-		for (int i = 0; i < M; i++) {
-			a.get(in.nextInt()).add(in.nextInt());
-		}
-
-		for (int i = 1; i < N+1; i++) {
-			if (d[i] == 0) {
-				dfs(i);
-				ans++;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				a[i][j] = in.nextInt();
 			}
-
 		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (land[i][j] == 0 && a[i][j] == 1) {
+					bfs(i, j, ++cnt);
+				}
+			}
+		}
+		broadBfs();
+		
+		int ans = -1;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < 4; k++) {
+					int x = i + dx[k];
+					int y = j + dy[k];
+					if (0 <= x && x < N && 0 <= y && y < N) {
+						if (land[i][j] != land[x][y] ) {
+							int temp = distance[i][j] + distance[x][y];
+							if (ans == -1 || ans > temp )
+								ans = temp;
+						}
+					}
+				}
+			}
+		}
+		
 		System.out.println(ans);
-	}
-
-	public static void dfs(int x) {
-		d[x] = 1;
-		for (int i = 0; i < a.get(x).size(); i++) {
-			int y = a.get(x).get(i);
-			if (d[y] == 0) {
-				d[y] = 1;
-				dfs(y);
-			}
-		}
 	}
 }
