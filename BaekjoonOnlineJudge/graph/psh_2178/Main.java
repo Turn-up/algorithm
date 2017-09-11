50372d9b2368e3b3d77d2ba68e1203e5a37313fd
ssunghooni <shcjswo89@naver.com>
2017-09-11 20:24:50 +0900

    단지번호 붙이기 - 두번째 푼 풀이

--- BaekjoonOnlineJudge/graph/psh_2178/Main.java
+++ BaekjoonOnlineJudge/graph/psh_2178/Main.java
@@ -1,64 +1,12 @@
 package psh_2178;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-/*
- * 문제        : https://www.acmicpc.net/problem/2178
- * 문제 종류  : 미로찾기 - 그래프
- * 작성자     : 박성훈
- * 작성일     : 2017-08-09
- * */
+
 public class Main {
 
-	private static int N;
-	private static int M;
-	private static int a[][];
-	private static int d[][];
-	private static int dx[] = { 0, 0, 1, -1 };
-	private static int dy[] = { 1, -1, 0, 0 };
-	
-	private static void bfs(int x, int y) {
-		Queue<PairXY> q = new LinkedList<>();
-		q.add(new PairXY(x, y));
-		d[1][1] = 1;
-		
-		while (!q.isEmpty()) {
-			PairXY xy = q.remove();
-			for (int i = 0; i < 4; i++) {
-				int nx = xy.x + dx[i];
-				int ny = xy.y + dy[i];
-				
-				if (1 <= nx && nx <= N && 1 <= ny && ny <= M) {
-					if (a[nx][ny] == 1 && d[nx][ny] == 0){
-						q.add(new PairXY(nx, ny));
-						d[nx][ny] = d[xy.x][xy.y] + 1;
-					}
-				}
-			}
-		}
-		
-		
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
-		N = in.nextInt();
-		M = in.nextInt();
-		a = new int[N + 1][M + 1];
-		d = new int[N + 1][M + 1];
-		
-		for (int i = 1; i <= N; i++) {
-			char[] temp = in.next().toCharArray();
-			for (int j = 1; j <= M; j++) {
-				a[i][j] = temp[j - 1] - '0';
-			}
-		}
-
-		bfs(1,1);
-		System.out.println(d[N][M]);
-	}
 	static class PairXY {
 		int x;
 		int y;
@@ -68,5 +16,66 @@ public class Main {
 			this.y = y;
 		}
 	}
-	
+
+	public static void bfs(boolean check[][], int data[][], int x, int y) {
+		Queue<PairXY> q = new LinkedList<>();
+		q.add(new PairXY(x, y));
+		check[x][y] = true;
+		while (!q.isEmpty()) {
+			int xPoint = q.peek().x;
+			int yPoint = q.remove().y;
+			int nowData = data[xPoint][yPoint];
+			if (nowData != 0) {
+				if (xPoint + 1 < data.length && data[xPoint + 1][yPoint] != 0 && check[xPoint + 1][yPoint] == false) {
+					q.add(new PairXY(xPoint + 1, yPoint));
+					data[xPoint + 1][yPoint] += nowData;
+					check[xPoint + 1][yPoint] = true;
+				}
+				if (xPoint - 1 >= 0 && data[xPoint - 1][yPoint] != 0 && check[xPoint - 1][yPoint] == false) {
+					q.add(new PairXY(xPoint - 1, yPoint));
+					data[xPoint - 1][yPoint] += nowData;
+					check[xPoint - 1][yPoint] = true;
+				}
+				if (yPoint + 1 < data[xPoint].length && data[xPoint][yPoint + 1] != 0
+						&& check[xPoint][yPoint + 1] == false) {
+					q.add(new PairXY(xPoint, yPoint + 1));
+					data[xPoint][yPoint + 1] += nowData;
+					check[xPoint][yPoint + 1] = true;
+				}
+				if (yPoint - 1 >= 0 && data[xPoint][yPoint - 1] != 0 && check[xPoint][yPoint - 1] == false) {
+					q.add(new PairXY(xPoint, yPoint - 1));
+					data[xPoint][yPoint - 1] += nowData;
+					check[xPoint][yPoint - 1] = true;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int m = in.nextInt();
+		boolean check[][] = new boolean[n][m];
+		int data[][] = new int[n][m];
+		// ArrayList <PairXY> pairXYList = new ArrayList<>();
+		String value;
+
+		for (int i = 0; i < n; i++) {
+			value = in.next();
+			for (int j = 0; j < m; j++) {
+				data[i][j] = value.charAt(j) - '0';
+				// pairXYList.add(new PairXY(i,j));
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (!check[i][j] && data[i][j] != 0) {
+					bfs(check, data, i, j);
+				}
+			}
+		}
+		System.out.println(data[n - 1][m - 1]);
+
+	}
 }
